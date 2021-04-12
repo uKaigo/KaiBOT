88505f97d71b449023b71f74bc6345150c254841
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-04-12 16:54:02 -0300

    Improve the roll command
    It's accepting dice count, and has some checks to prevent abuse.

--- kaibot/cogs/utilities.py
+++ kaibot/cogs/utilities.py
@@ -1,10 +1,11 @@
-from secrets import randbelow
-import discord
+import re
+from random import randint
 
+import discord
 from discord.ext import commands
 
 from .. import config
-from ..utils import custom
+from ..utils import custom, format_list
 from ..i18n import Translator
 
 _ = Translator(__name__)
@@ -14,33 +15,60 @@ class Utilities(custom.Cog, translator=_):
     """Comandos úteis."""
 
     def __init__(self, bot):
+        self.DICE_REGEX = re.compile(r"((?P<count>\d*)d)?(?P<sides>\d+(?!\d*d))")  # Regex Sucks
         self.bot = bot
 
     @commands.command()
-    async def roll(self, ctx, sides: int):
+    async def roll(self, ctx, dice="d20"):
         """
-        Rola um dado de `SIDES` lados.
+        Rola um dado.
 
-        O dado precisa ter pelo menos 2 lados.
+        Explicação (`count`d`sides`):
+        `5d100` irá rodar 5 dados de 100 lados.
+        `d100` irá rodar 1 dado de 100 lados.
+        `100` irá rodar 1 dado de 100 lados.
         """
-        if sides < 2:
-            return await ctx.send(_('Insira um dado de pelo menos 2 lados.'))
+        match = self.DICE_REGEX.match(dice)
+        if not match:
+            return await ctx.send_help(ctx.command)
 
-        result = randbelow(sides + 1) or 1
-        return await ctx.send(_('Seu dado resultou em: {result}', result=result))
+        count = int(match.group("count") or 1)
+        sides = int(match.group("sides"))
+
+        if 0 in (count, sides):
+            return await ctx.send(_("Nenhum dos valores pode ser 0."))
+        if sides == 1:
+            return await ctx.send(_("O dado não pode ter somente 1 lado."))
+        if sides > 1000:
+            return await ctx.send(_("O dado precisa ter menos que 1000 lados."))
+        if count > 200:
+            return await ctx.send(_("Você pode rolar no máximo 200 dados por vez."))
+
+        results = []
+        for i in range(count):
+            results.append(randint(1, sides))
+
+        if count == 1:
+            return await ctx.send(_("Seu dado resultou em: {result}", result=results[0]))
+
+        return await ctx.send(
+            _("Seus dados resultaram em: {results}", results=format_list(results))
+            + "\n\n"
+            + _("Soma: {sum}", sum=sum(results))
+        )
 
     @commands.command()
     async def resolve(self, ctx, link):
         """Mostra todos os redirecionamentos do link."""
-        embed = discord.Embed(title=_('Redirecionamentos'), color=config.MAIN_COLOR)
+        embed = discord.Embed(title=_("Redirecionamentos"), color=config.MAIN_COLOR)
 
-        description = ''
+        description = ""
         async with ctx.typing():
-            dest = await self.bot.session.get(link.strip('<>'))
+            dest = await self.bot.session.get(link.strip("<>"))
 
             for index, res in enumerate(dest.history + (dest,)):
-                em = '🔷' if index in (0, len(dest.history)) else '🔹'
-                description += f'{em} `{res.status}` - {res.url}\n'
+                em = "🔷" if index in (0, len(dest.history)) else "🔹"
+                description += f"{em} `{res.status}` - {res.url}\n"
 
         embed.description = description
 
