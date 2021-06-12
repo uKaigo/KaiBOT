6475ed90a71e40431537a4c43b8d8081bb9c3314
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-06-12 03:08:15 -0300

    Consider someone in prompt state as playing

--- kaibot/cogs/fun.py
+++ kaibot/cogs/fun.py
@@ -19,6 +19,7 @@ class Fun(custom.Cog, translator=_):
     def __init__(self, bot):
         self.bot = bot
         self._ttt_game = TTTIntegration(bot)
+        self.waiting = []
 
     def cog_unload(self):
         self._ttt_game.destroy()
@@ -34,7 +35,7 @@ class Fun(custom.Cog, translator=_):
             return await ctx.send(_('Você não pode jogar com você mesmo.'))
         if ctx.author.id in self._ttt_game:
             return await ctx.send(_('Você já está em um jogo.'))
-        if player.id in self._ttt_game:
+        if player.id in self._ttt_game or player.id in self.waiting:
             return await ctx.send(
                 _('**{player}** já está em um jogo.', player=escape_text(player.display_name))
             )
@@ -50,10 +51,12 @@ class Fun(custom.Cog, translator=_):
 
         check = lambda r, u: r.message == msg and u == player and r.emoji in ['✅', '❌']
         try:
+            self.waiting.append(player.id)
             reaction, user = await self.bot.wait_for('reaction_add', check=check, timeout=30)
         except TimeoutError:
             return await msg.edit(content=text + '\n- ' + _('Tempo excedido.'))
         finally:
+            self.waiting.remove(player.id)
             if ctx.me.permissions_in(ctx.channel).manage_messages:
                 await msg.clear_reactions()
 
