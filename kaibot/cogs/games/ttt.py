7d4497380053ea4d839f09460d9f9cc98ecc859c
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-06-14 01:29:25 -0300

    Use buttons instead of reactions in ttt

--- kaibot/cogs/games/ttt.py
+++ kaibot/cogs/games/ttt.py
@@ -1,24 +1,13 @@
 import asyncio
 from enum import IntEnum
-from itertools import chain
 from typing import NamedTuple
 
 import discord
+import discord.http
 
-from ... import config
 from ...i18n import Translator
 
 _ = Translator(__name__)
-
-# DISPLAY #
-NUMBERS = ('1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣')
-TTT_GRID = '''
-⁣{}❕{}❕{}
-➖➕➖➕➖
-{}❕⁣{}❕{}
-➖➕➖➕➖
-{}❕{}❕{}
-'''
 
 # State Helpers #
 
@@ -104,6 +93,9 @@ class TTTImplementation:
 
 
 # Integration #
+CUSTOM_ID_PREFIX = 'btn_'
+
+
 class TTTIntegration:
     def __init__(self, bot):
         self.bot = bot
@@ -125,10 +117,6 @@ class TTTIntegration:
         self.__tasks.append(task)
         task.add_done_callback(lambda f: self.__tasks.remove(task))
 
-    async def _add_reactions(self, msg, game):
-        for move in game.valid_moves:
-            await msg.add_reaction(NUMBERS[move - 1])
-
     async def _do_rollover(self, message):
         channel = message.channel
         if channel.last_message == message:
@@ -143,23 +131,77 @@ class TTTIntegration:
             return await channel.send('\N{ZERO WIDTH SPACE}')
         return message
 
-    def _get_check(self, message, game, players):
-        def predicate(reaction, user):
-            checks = (
-                user.id in self.games,
-                reaction.emoji in NUMBERS,
-                message.id == reaction.message.id,
-            )
-            return all(checks) and game.turn == Players(players.index(user))
+    async def ack_interaction(self, interaction_id, interaction_token):
+        route = discord.http.Route(
+            'POST',
+            '/interactions/{interaction_id}/{interaction_token}/callback',
+            interaction_id=interaction_id,
+            interaction_token=interaction_token,
+        )
 
-        return predicate
+        payload = {'type': 6}
+
+        await self.bot.http.request(route, json=payload)
+
+    async def get_move_for(self, msg_id, player):
+        while True:
+            # Something is wrong if we don't receive a single event in 1
+            # minute, but let's keep it here.
+            event = await self.bot.wait_for('socket_response', timeout=60)
+
+            if event['t'] != 'INTERACTION_CREATE':
+                continue
+
+            payload = event['d']
+
+
+            if not payload.get('message') or payload['message']['id'] != str(msg_id):
+                await self.ack_interaction(payload['id'], payload['token'])
+                continue
+
+            if not payload.get('member') or payload['member']['user']['id'] != str(player.id):
+                await self.ack_interaction(payload['id'], payload['token'])
+                continue
+
+            index = payload['data']['custom_id'][len(CUSTOM_ID_PREFIX) :]
+
+            await self.ack_interaction(payload['id'], payload['token'])
+
+            return int(index) - 1
+
+    async def edit_message(self, msg, txt, game):
+        route = discord.http.Route(
+            'PATCH',
+            '/channels/{channel_id}/messages/{message_id}',
+            channel_id=msg.channel.id,
+            message_id=msg.id,
+        )
+
+        payload = {'content': txt, 'allowed_mentions': self.bot.allowed_mentions.to_dict()}
+        payload['components'] = []
+
+        for column, rows in enumerate(game.table):
+            btn_row = []
+            for row, value in enumerate(rows):
+                base = {'type': 2, 'custom_id': CUSTOM_ID_PREFIX + str((3 * column + 1) + row)}
+                if value == Players.UNSET:
+                    base['style'] = 2
+                    base['label'] = '\u200b     \u200b'
+                elif value == Players.X:
+                    base['style'] = 1
+                    base['label'] = 'X'
+                elif value == Players.O:
+                    base['style'] = 4
+                    base['label'] = 'O'
+                btn_row.append(base)
+            payload['components'].append({'type': 1, 'components': btn_row})
+
+        await self.bot.http.request(route, json=payload)
 
     # GAME
 
     async def start(self, message, player_x, player_o):
         game = TTTImplementation()
-
-        self._create_task(self._add_reactions(message, game))
 
         info = GameInfo(message, game, (player_x, player_o))
         self.games[player_x.id] = info
@@ -176,57 +218,34 @@ class TTTIntegration:
                 # works.
                 message = self.games[players[0].id][0]
 
-                check = self._get_check(message, game, players)
-                reaction, _ = await bot.wait_for('reaction_add', check=check, timeout=60 * 3)
+                player = players[game.turn]
+                move = await asyncio.wait_for(self.get_move_for(message.id, player), timeout=60 * 2)
 
-                move = NUMBERS.index(reaction.emoji)
                 try:
                     game.make_move(move)
                 except ValueError:
                     continue
-                else:
-                    if message.guild.me.permissions_in(message.channel).manage_messages:
-                        self._create_task(message.clear_reaction(NUMBERS[move]))
 
                 await self._update_game(message, game, players)
         except asyncio.TimeoutError:
-            return
+            return await self.games[players[0].id][0].edit(content='Tempo excedido.')
         finally:
             del self.games[players[0].id], self.games[players[1].id]
 
     async def _update_game(self, message, game, players):
-        symbols = ('❌', '⭕')
-        emojis = []
-        for k, v in enumerate(chain(*game.table)):
-            if v != Players.UNSET:
-                emojis.append(symbols[v])
-            else:
-                emojis.append(NUMBERS[k])
-
-        table = TTT_GRID.format(*emojis)
-
-        embed = discord.Embed(
-            title=_('Jogo da velha'),
-            description=table,
-            color=config.MAIN_COLOR,
-        )
-        embed.description += '\n'
-
         winner = game.winner
         if winner is None:
-            embed.description += _('Vez de {player}.', player=players[game.turn].mention)
+            txt = _('Vez de {player}.', player=players[game.turn].mention)
         else:
             if winner == Players.UNSET:
-                embed.description += _('Deu velha!')
+                txt = _('Deu velha!')
             else:
-                embed.description += _('{player} ganhou!', player=players[winner].mention)
+                txt = _('{player} ganhou!', player=players[winner].mention)
 
         msg = await self._do_rollover(message)
 
         if msg != message and game.winner is None:
-            self._create_task(self._add_reactions(msg, game))
-
             for player in players:
                 self.games[player.id] = (msg, game, players)
 
-        await msg.edit(content=None, embed=embed)
+        await self.edit_message(msg, txt, game)
