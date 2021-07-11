e51041a51750138b5a6123b18488d7bbf42354f6
uKaigo <37600692+uKaigo@users.noreply.github.com>
2021-07-11 00:19:20 -0300

    Don't inherit Enum

--- kaibot/utils/enums.py
+++ kaibot/utils/enums.py
@@ -1,9 +1,9 @@
-from enum import Enum, IntEnum
+from enum import IntEnum
 
 import discord
 
 
-class Emotes(Enum):
+class Emotes:
     X = discord.PartialEmoji(name='X_', id=863602074285375520)
     O = discord.PartialEmoji(name='O_', id=863602074545553418)
     FIRST = discord.PartialEmoji(name='first', id=863600652881494036)
@@ -12,6 +12,7 @@ class Emotes(Enum):
     NEXT = discord.PartialEmoji(name='next', id=863600653636993055)
     LAST = discord.PartialEmoji(name='last', id=863600653972144178)
     QUESTION = discord.PartialEmoji(name='question_', id=863600654740094977)
+
 
 class Flags(IntEnum):
     VIP = 1
